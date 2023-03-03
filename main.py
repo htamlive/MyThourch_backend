6fbdeb3d195b559ef79dcb27b4e2cd2714836721
chulanpro5 <chulanpro5@gmail.com>
2023-03-03 21:29:30 +0700

    test new main

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+"""
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -63,3 +64,18 @@ if __name__ == '__main__':
     app.run(debug=True)
 
 
+"""
+
+from flask import Flask, jsonify
+import os
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index():
+    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
+
+
+if __name__ == '__main__':
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
