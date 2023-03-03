b360dac2e78a9b21f1af8e62fdd6774ce42a053e
chulanpro5 <chulanpro5@gmail.com>
2023-03-03 21:51:30 +0700

    add railway config file

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-"""
 import os
 from dotenv import load_dotenv
 load_dotenv()
@@ -62,20 +61,3 @@ def listen_user():
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-"""
-
-from flask import Flask, jsonify
-import os
-
-app = Flask(__name__)
-
-
-@app.route('/')
-def index():
-    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
-
-
-if __name__ == '__main__':
-    app.run(debug=True, port=os.getenv("PORT", default=5000))
