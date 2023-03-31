6bb9712da5bf7e3d662fa0c2b6a369ca88d0b501
htamlive <nghichmaychoi@gmail.com>
2023-03-31 19:40:51 +0700

    add searching title features

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 #%%
 import os
 from dotenv import load_dotenv
-load_dotenv('.env.template')
+load_dotenv()
 REDIS_ACTIVATE = os.getenv("REDIS_ACTIVATE")
 print(REDIS_ACTIVATE)
 
@@ -10,6 +10,7 @@ from src.crawl_data import crawl_url
 from src.StageSender import StageSender
 
 from flask import Flask,request
+import requests
 import json
 from flask_cors import CORS
 import openai
@@ -18,6 +19,16 @@ from flask_socketio import SocketIO, emit
 #%%
 # print(os.environ["OPENAI_API_KEY"])
 # print(os.getenv("REDIS_HOST"))
+
+def get_title_from_url(url):
+    response = requests.get(url)
+    # Extract the page title from the HTML content using string manipulation
+    title_start = response.text.find("<title>") + len("<title>")
+    title_end = response.text.find(" - Wikipedia</title>")
+    title = response.text[title_start:title_end]
+
+    # Print the page title
+    return title
 
 app = Flask(__name__)
 CORS(app)
@@ -37,20 +48,46 @@ def hello_world():
 
 @app.route('/api/wiki_retrieve/', methods=['POST'])
 def listen_url():
-    print(request[:10] + '... ')
+    print(request)
     url = request.json['url']
+    title = get_title_from_url(url)
     openai.api_key = request.json['apiKey']
 
     stageSender.send_stage("Crawling data from " + url)
 
-    payload = documentInteraction.insert_and_process_document(crawl_url(url))
+    payload, topics = documentInteraction.insert_and_process_document(crawl_url(url))
 
     stageSender.send_stage("Crawling data from " + url + " Done")
-    print(payload[:10] + '... ')
+    print(payload)
     # payload = [["AI is used to show intelligence in activities such as speech recognition, computer vision, and language translation"], ["Examples of AI applications include web search engines (Google Search), recommendation systems (YouTube, Amazon, Netflix), understanding human speech (Siri, Alexa), self-driving cars (Waymo), generative or creative tools (ChatGPT, AI art), automated decision-making and strategic game systems (chess, Go)"], ["AI is used in a wide range of topics and activities"]]
     response = {
-        "payload" : json.dumps(payload)
+        "payload" : json.dumps(payload),
+        "topics" : json.dumps(topics),
+        "title": title
     }
+    return response
+
+@app.route('/api/test_wiki_retrieve/', methods=['POST'])
+def test_listen_url():
+    print(request)
+    url = request.json['url']
+    title = get_title_from_url(url)
+    openai.api_key = request.json['apiKey']
+
+    stageSender.send_stage("Crawling data from " + url)
+
+    #payload, topics = documentInteraction.insert_and_process_document(crawl_url(url))
+
+    stageSender.send_stage("Crawling data from " + url + " Done")
+
+    #print(payload)
+    # payload = [["AI is used to show intelligence in activities such as speech recognition, computer vision, and language translation"], ["Examples of AI applications include web search engines (Google Search), recommendation systems (YouTube, Amazon, Netflix), understanding human speech (Siri, Alexa), self-driving cars (Waymo), generative or creative tools (ChatGPT, AI art), automated decision-making and strategic game systems (chess, Go)"], ["AI is used in a wide range of topics and activities"]]
+    
+    #open json file
+    with open(r'test\wiki_test.json') as json_file:
+        response = json.load(json_file)
+    
+    
     return response
 
 
